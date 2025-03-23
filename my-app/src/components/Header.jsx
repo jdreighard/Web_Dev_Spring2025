2f24906b6d9289621c72a08f5636281ed9184902
JD <jd@tempesttech.com>
2025-03-23 15:52:22 -0600

    Styled nav bar, created hamburger svg, started setup for useState for loading in mobile menu view (got rid of duplicate files as well)

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+
 function Header() {
+  const [mobileNavOpen, setMobileNaveOpen] = useState(false); // variables for 
   return (
-    <header>
-      <img src ="/header_img.svg" alt ="Tech Image" width = "200px" height="200px"></img>
-      <nav> 
-        <ul>
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-          <li>TEST</li>
-        </ul>
-      </nav>
+    <header className="fixed top-0 w-full flex flex-row h-40 bg-#121111">
+      <div className="w-full max-w-screen-xl m-auto flex items-center">
+        <img src ="/header_img.svg" alt ="Tech Image" width = "75px" height="75px" className="ml-10"></img>
+        <nav className="w-full hidden md:flex">  
+          <ul className ="w-full flex flex-row justify-around items-center text-white"> 
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            <li>TEST</li>
+          </ul>
+        </nav>
+        <button className="bg-[#489C24] text-white py-2 px-6 rounded-full hover:bg-[#3f7b1e] focus:outline-none focus:ring-2 focus:ring-green-300">Login</button>
+      </div>
     </header>
   )
 }
