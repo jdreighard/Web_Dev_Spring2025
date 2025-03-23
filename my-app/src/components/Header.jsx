fe06e688908f9e8a284a9bdc3716568427db9cde
JD <jd@tempesttech.com>
2025-03-23 01:25:25 -0600

    created header svg, created function for header as well (TODO: Styling)

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src ="/header_img.svg" alt ="Tech Image" width = "200px" height="200px"></img>
+      <nav> 
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+          <li>TEST</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header;
