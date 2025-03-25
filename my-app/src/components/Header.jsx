74b72fb953b5b0ba48c60cf8c3ab9ddcf272850a
JD <jd@tempesttech.com>
2025-03-25 10:43:49 -0600

    minor changes to nav bar for mobile (create login image for button, small layout tweaks for mobile)

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="fixed top-0 w-full h-40 bg-[#121111]">
       {/*Main header row*/}
-      <div className="w-full max-w-screen-xl m-auto flex items-center justify-between p-3 h-9">
+      <div className="w-full max-w-screen-xl m-auto flex items-center justify-between p-3 h-auto mt-2rem">
         <img src ="/header_img.svg" alt ="Tech Image" width = "50px" height="50px"/>
         {/* Desktop nav bar section */}
         <nav className="w-full hidden md:flex">  
@@ -19,12 +19,20 @@ function Header() {
           </ul>
         </nav>
         {/* Mobile nav bar section*/}
-        <button 
-          onClick ={() => setMobileNavOpen(!mobileNavOpen)}
-          className="md:hidden">
-          <img src="/hamburger.svg" alt="navigation menu" width="40px" height="40px"/>
-        </button> 
-        <button className="bg-[#489C24] text-white py-2 px-6 rounded-full hover:bg-[#3f7b1e] focus:outline-none focus:ring-2 focus:ring-green-300">Login</button>
+        <div className="flex justify-center content-center m-auto">
+          <button 
+            onClick ={() => setMobileNavOpen(!mobileNavOpen)}
+            className="md:hidden justify-center">
+            <img src="/hamburger.svg" alt="navigation menu" width="40px" height="40px"/>
+          </button> 
+        </div>
+        
+        {/*Show this button on medium screens and above*/}
+        <button className=" hidden md:block bg-[#489C24] text-white py-2 px-6 rounded-full hover:bg-[#3f7b1e] focus:outline-none focus:ring-2 focus:ring-green-300">Login</button>
+        {/*Show the picture for the button on mobile*/}
+        <button className="md:hidden">
+        <img src="/login.svg" alt="Login" className="w-50px h-50px" />
+        </button>
       </div>
       {mobileNavOpen && <MobileNav />}
     </header>
