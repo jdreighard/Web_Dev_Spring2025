f643f0ec3ff498d484a9c643650d65118276c15e
JD <jd@tempesttech.com>
2025-03-27 10:44:40 -0600

    header rendering stuff push (Changed box sizing on different viewports as well)

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,41 +1,66 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MobileNav from './MobileNav'; // Import needed for mobile hamburger menu
 
 function Header() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false); // variables for mobile menu 
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) { 
+        setShowHeader(false);
+      } 
+      else {
+          setShowHeader(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 w-full h-20 bg-[#121111]">
-      {/*Main header row*/}
-      <div className="w-full max-w-screen-xl m-auto flex items-center justify-between p-3 h-auto mt-2rem">
-        <img src ="/header_img.svg" alt ="Tech Image" width = "50px" height="50px"/>
-        {/* Desktop nav bar section */}
-        <nav className="w-full hidden md:flex">  
-          <ul className ="w-full flex flex-row justify-around items-center text-white"> 
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>CONTACT</li>
-            <li>TEST</li>
-          </ul>
-        </nav>
-        {/* Mobile nav bar section*/}
-        <div className="flex justify-center content-center m-auto">
-          <button 
-            onClick ={() => setMobileNavOpen(!mobileNavOpen)}
-            className="md:hidden justify-center">
-            <img src="/hamburger.svg" alt="navigation menu" width="40px" height="40px"/>
-          </button> 
+    <div>
+      {showHeader &&(
+      <header className="fixed top-0 w-full h-20 bg-[#121111]">
+        {/*Main header row*/}
+        <div className="w-full max-w-screen-xl m-auto flex items-center justify-between p-3 md:px-12 h-auto mt-2rem">
+          <img src ="/header_img.svg" alt ="Tech Image" width = "50px" height="50px"/>
+          {/* Desktop nav bar section */}
+          <nav className="w-full hidden md:flex">  
+            <ul className ="w-full flex flex-row justify-around items-center text-white font-bold"> 
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+              <li>TEST</li>
+            </ul>
+          </nav>
+          {/* Mobile nav bar section*/}
+          <div className="flex justify-center content-center m-auto">
+            <button 
+              onClick ={() => setMobileNavOpen(!mobileNavOpen)}
+              className="md:hidden justify-center">
+              <img src="/hamburger.svg" alt="navigation menu" width="40px" height="40px"/>
+            </button> 
+          </div>
+          
+          {/*Show this button on medium screens and above*/}
+          <button className=" hidden md:block bg-[#489C24] text-white py-2 px-6 rounded-full hover:bg-[#3f7b1e] focus:outline-none focus:ring-2 focus:ring-green-300">Login</button>
+          {/*Show the picture for the button on mobile*/}
+          <button className="md:hidden">
+          <img src="/login.svg" alt="Login" className="w-50px h-50px" />
+          </button>
         </div>
-        
-        {/*Show this button on medium screens and above*/}
-        <button className=" hidden md:block bg-[#489C24] text-white py-2 px-6 rounded-full hover:bg-[#3f7b1e] focus:outline-none focus:ring-2 focus:ring-green-300">Login</button>
-        {/*Show the picture for the button on mobile*/}
-        <button className="md:hidden">
-        <img src="/login.svg" alt="Login" className="w-50px h-50px" />
-        </button>
-      </div>
-      {mobileNavOpen && <MobileNav />}
-    </header>
+        {mobileNavOpen && <MobileNav />}
+      </header>
+    )}
+
+  </div>
+    
   )
 }
 export default Header;
--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -7,13 +7,13 @@ function Layout() {
         <main className ="pt-40"> 
           <div className="max-w-screen-xl w-11/12 mx-auto bg-[#17171A]">  
             <div className="w-full flex justify-center items center flex-col">
-              <div className="w-[80%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
+              <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
                 <h3 className="font-bold">Content</h3>
               </div>
-              <div className="w-[80%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
+              <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
                 <h3 className="font-bold">Content</h3>
               </div>
-              <div className="w-[80%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
+              <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
                 <h3 className="font-bold">Content</h3>
               </div>
             </div>
