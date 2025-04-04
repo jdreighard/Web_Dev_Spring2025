9bb19825ee091b75819f2506692768c0b3a7ed0f
JD <jd@tempesttech.com>
2025-04-04 11:53:09 -0600

    started main galleries for hompage

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -7,8 +7,10 @@ function Layout() {
         <main className ="pt-40"> 
           <div className="max-w-screen-xl w-11/12 mx-auto bg-[#17171A]">  
             <div className="w-full flex justify-center items center flex-col">
-              <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
-                <h3 className="font-bold">Content</h3>
+              <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex flex-col justify-center items-center p-4 m-6 h-auto custom-shadow-green">
+                <h3 className="font-bold">Fishing Gallery</h3>
+                <img src="/main_fish.jpg" alt="fish picture" className="w-[90%] md:w-[80%] lg:w-[76%] pt-4 mt-4 rounded-md"/>
+                <button className="p-4 m-6 bg-[#489C24]">More</button>
               </div>
               <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] mx-auto bg-[#1F1F23] flex justify-center items-center p-4 m-6 h-72 custom-shadow-green">
                 <h3 className="font-bold">Content</h3>
--- my-app/src/components/MobileNav.jsx
+++ my-app/src/components/MobileNav.jsx
@@ -3,7 +3,7 @@ function MobileNav() {
   return (
     <nav className="md:hidden bg-[#121111]">
       <div className="w-full m-auto bg-[#17171a] h-12">
-        <ul className ="w-full flex flex-row justify-around items-center text-white h-12">
+        <ul className ="w-full flex flex-row justify-around items-center text-white font-bold h-12">
           <li>HOME</li>
           <li>ABOUT</li>
           <li>CONTACT</li>
